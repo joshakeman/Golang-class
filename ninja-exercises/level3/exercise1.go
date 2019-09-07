68439bac9b4e8483b5eb95333ce5d856a3ffeda7
Josh Akeman <joshakeman@gmail.com>
2019-09-07 11:09:38 -0400

    did another block of exercises

--- ninja-exercises/level3/exercise1.go
+++ ninja-exercises/level3/exercise1.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 )
 
 func main() {
-
-	fmt.Println("OS\t", runtime.GOOS)
-	fmt.Println("ARCH\t", runtime.GOARCH)
-	fmt.Println("CPUs\t", runtime.NumCPU())
-	fmt.Println("Goroutines\t", runtime.NumGoroutine())
-
 }
 //
